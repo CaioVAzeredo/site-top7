62a01a8b16514c6105baeec3d2dd433b78048ac0
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-05-01 21:04:26 -0300

    adição as informações do header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CabecalhoComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // <-- aqui estava errado
 })
 export class AppComponent {
   title = 'site-top7';
 }
+
