149035d592958edef748fd98a27b386e14c0b94b
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-05-02 14:22:01 -0300

    adição do estilo Modalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
 import { HeroComponent } from "./componentes/hero/hero.component";
 import { ModalidadesComponent } from "./componentes/modalidades/modalidades.component";
+import { SobreNosComponent } from "./componentes/sobre-nos/sobre-nos.component";
+import { MatriculasComponent } from "./componentes/matriculas/matriculas.component";
+import { EquipeComponent } from "./componentes/equipe/equipe.component";
+import { ContatoComponent } from "./componentes/contato/contato.component";
+import { RodapeComponent } from "./componentes/rodape/rodape.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, HeroComponent, ModalidadesComponent],
+  imports: [CabecalhoComponent, HeroComponent, ModalidadesComponent, SobreNosComponent, MatriculasComponent, EquipeComponent, ContatoComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // <-- aqui estava errado
 })
