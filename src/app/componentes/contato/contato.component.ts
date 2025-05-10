6719323c784e5e639e4cc491ab43ad15144c6be0
Caio VAzeredo <caio.viana.39@gmail.com>
2025-05-10 19:44:10 -0300

    adição da logica do formulario

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponentComponent } from '../button-component/button-component.component';
 
 @Component({
   selector: 'app-contato',
-  imports: [CommonModule, ButtonComponentComponent],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule, 
+    ButtonComponentComponent
+  ],
   templateUrl: './contato.component.html',
   styleUrl: './contato.component.css'
 })
-export class ContatoComponent {
-  form(event: Event) {
-    event.preventDefault();
-    alert('Formulario enviado');
+export class ContatoComponent implements OnInit {
+  contatoForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.inicializarFormulario();
+  }
+
+  inicializarFormulario() {
+    this.contatoForm = new FormGroup({
+      assunto: new FormControl('', Validators.required),
+      nome: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      mensagem: new FormControl('', Validators.required),
+    });
+  }
+
+  salvarContato() {
+    if (this.contatoForm.valid) {
+      console.log(this.contatoForm.value);
+      alert('Formulário enviado com sucesso!');
+      this.contatoForm.reset();
+    } else {
+      this.contatoForm.markAllAsTouched();
+    }
   }
 }
