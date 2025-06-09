c58621a28811a3da1bccaf33323a9cd402313c6b
Caio Viana de Azeredo <caio.viana.39@gmail.com>
2025-06-09 14:19:41 -0300

    adição da rota para home na logo e no botao Home

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
@@ -7,9 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './cabecalho.component.css'
 })
 export class CabecalhoComponent {
+
+  constructor(private router: Router) { }
   menuAberto = false;
   statusMenu() {
     this.menuAberto = !this.menuAberto;
+  }
+  pagHome() {
+    this.router.navigate(['/']);
   }
   cart() {
     alert("Carrinho de compras clicado!");
