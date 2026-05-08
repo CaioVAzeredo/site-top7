ee9bcaf274682673eb68b0128cb995fc6c17bb2b
CaioVAzeredo <caio.viana.39@gmail.com>
2026-05-08 20:43:58 -0300

    feat: adiciona informacoes sobre a colonia de ferias completo

--- src/app/componentes/colonia-de-ferias/colonia-de-ferias.component.ts
+++ src/app/componentes/colonia-de-ferias/colonia-de-ferias.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { ApiService } from '../../service/api.service';
+import { Escola } from '../../models/escolas.model';
 
 @Component({
   selector: 'app-colonia-de-ferias',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './colonia-de-ferias.component.html',
   styleUrl: './colonia-de-ferias.component.css'
 })
-export class ColoniaDeFeriasComponent {
 
+export class ColoniaDeFeriasComponent {
+  escolas: Escola[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getEscolas().subscribe(res => {
+      this.escolas = res;
+    });
+  }
+
+  abrirLinkExterno(escola: Escola): void {
+    if (escola.nome.toLowerCase().includes('sigma')) {
+      window.open('https://docs.google.com/forms/d/1FgJZR7QXK1fbg17gABSQXd4vyXSC_SwVqPpEf4Ysp4Y/edit?ts=69fa7e2d', '_blank');
+    }
+
+    if (escola.nome.toLowerCase().includes('ideal')) {
+      window.open('https://docs.google.com/forms/d/1E1t4m3u4XCtJMRw_GOnx2VQKkMBiIYFL97eabxcexhM/edit?usp=sharing_eil_se_dm&ts=69fa93b9', '_blank');
+    }
+  }
+
+  trackByEscolaId(index: number, item: Escola): number {
+    return Number(item.id);
+  }
 }
